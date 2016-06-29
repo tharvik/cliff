d014ec3d1c096a533056fdeb8e330a9d4d6237f4
tharvik <tharvik@users.noreply.github.com>
2016-06-29 10:05:50 +0200

    handle annotations in function

--- cliff/app.py
+++ cliff/app.py
@@ -8,6 +8,7 @@ import locale
 import logging
 import logging.handlers
 import os
+import six
 import sys
 
 from .complete import CompleteCommand
@@ -298,7 +299,11 @@ class App(object):
             return 2
         cmd_factory, cmd_name, sub_argv = subcommand
         kwargs = {}
-        if 'cmd_name' in inspect.getargspec(cmd_factory.__init__).args:
+        if six.PY2:
+            argspec = inspect.getargspec
+        else:
+            argspec = inspect.getfullargspec
+        if 'cmd_name' in argspec(cmd_factory.__init__).args:
             kwargs['cmd_name'] = cmd_name
         cmd = cmd_factory(self, self.options, **kwargs)
         err = None
